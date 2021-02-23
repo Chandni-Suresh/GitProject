2d7822f49908853b8f648a98c42fa5088febcadd
Chandni Suresh <chandnisuresh0224@gmail.com>
2021-02-22 19:37:31 -0800

    Inserted fields in Person class

--- src/com/test/Person.java
+++ src/com/test/Person.java
@@ -2,4 +2,40 @@ package com.test;
 
 public class Person {
 
+    private String firstName;
+    private String lastName;
+    private int age;
+    public Person()
+    {}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String firstName, String lastName, int age)
+    {
+        this.age=age;
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
 }
